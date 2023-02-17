ec8af2d56d620b84ced30316db3ffd403cc27155
Jungu Lee <1zzangjun@gmail.com>
2023-02-17 19:45:57 +0900

    :sparkles: helmet 추가

--- server/app.ts
+++ server/app.ts
@@ -1,13 +1,19 @@
 import express from "express";
 import bodyParser from "body-parser";
 import userRouter from "./routers/userRoute";
+import helmet from "helmet";
+
 import HttpError from "./models/error";
 
 const app = express();
+// 헬맷 설정 필요
+app.use(helmet());
 app.use(bodyParser.json());
+
 app.get("/", (req, res, next) => {
   console.log("접속함");
   res.json({ message: "안녕" });
+  next();
 });
 app.use("/user", userRouter);
 
