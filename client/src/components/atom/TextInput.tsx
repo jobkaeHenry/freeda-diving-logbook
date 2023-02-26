0841d75c87dace8b31ea801e3824854f44dd2321
Jungu Lee <1zzangjun@gmail.com>
2023-02-26 21:08:21 +0900

    :art: search bar 인풋

---
+++ client/src/components/atom/TextInput.tsx
@@ -0,0 +1,61 @@
+import { css } from "@emotion/react";
+import styled from "@emotion/styled";
+import React, { InputHTMLAttributes } from "react";
+import Icon from "./Icon";
+
+interface Props extends InputHTMLAttributes<HTMLInputElement> {
+  icon?: any;
+}
+
+const TextInput = (props: Props) => {
+  const Icon = props.icon;
+  return (
+    <InputWrapper css={css`position:relative;`}>
+      <Input
+        css={css`
+          padding-right: 32px;
+        `}
+        {...props}
+      />
+      {Icon ? (
+        <Icon
+          css={css`
+            position: absolute;
+            right: 8px;
+          `}
+        />
+      ) : (
+        <></>
+      )}
+    </InputWrapper>
+  );
+};
+
+const Input = styled.input`
+  &[type="text"] {
+    padding: 16px;
+    padding-right: ${(props: Props) => (props.icon ? "48px" : "")};
+    outline-style: solid;
+    outline-width: 1px;
+    width: 100%;
+    outline-color: var(--line-gray);
+    border-radius: 6px;
+    resize: none;
+    accent-color: var(--main);
+  }
+  &[type="text"]:disabled {
+    background-color: #eee;
+    color: var(--font-light-gray);
+  }
+  &[type="text"]:focus {
+    filter: drop-shadow(0px 0px 2px var(--main));
+    color: var(--font-main);
+  }
+`;
+
+const InputWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+export default TextInput;
--- client/src/features/newlogs/pages/LocationForm.tsx
+++ client/src/features/newlogs/pages/LocationForm.tsx
@@ -1,33 +1,55 @@
 import Radio from "@/components/atom/Radio";
 import Text from "@/components/atom/Text";
+import TextInput from "@/components/atom/TextInput";
+import SearchIcon from '../../../assets/searchIcon.svg'
 import { DiveLogTypes } from "@/types/DiveLogTypes";
-import React, { useState } from "react";
+import { useTranslation } from "react-i18next";
 import FormLayout from "../layout/FormLayout";
+import { WeatherTypeGuard } from "../utils/typeguard";
 
-type Props = Pick<DiveLogTypes, "location"> & {
-  updateFields: (field: Pick<DiveLogTypes, "location">) => void;
-};
+type Props = Pick<DiveLogTypes, "location"> &
+  Pick<DiveLogTypes, "weather"> & {
+    updateFields: (
+      field: Pick<DiveLogTypes, "location"> | Pick<DiveLogTypes, "weather">
+    ) => void;
+  };
 
-const LocationForm = ({ location, updateFields }: Props) => {
-
+const LocationForm = ({ location, weather, updateFields }: Props) => {
+  const { t } = useTranslation(["diveForm", "common"]);
   return (
     <FormLayout>
       {/* 타이틀 */}
       <FormLayout.Title>
         <Text typography="h1" bold as={"h1"}>
-          어디에서 활동하셨나요?
+          {t("어디에서 활동하셨나요?")}
         </Text>
       </FormLayout.Title>
       {/* 메인 */}
       <FormLayout.Main>
-        <input
+        <TextInput
+          icon={SearchIcon}
           type="text"
           required
+          placeholder={t("common:검색하기") || "Search"}
           defaultValue={location.title}
-          onChange={(e) =>
-            updateFields({ location: { ...location, title: e.target.value } })
-          }
+          onClick={() => {}}
+          // onChange={(e) =>
+          //   updateFields({ location: { ...location, title: e.target.value } })
+          // }
         />
+        <Radio
+          value={weather.weather}
+          onChange={(e) => {
+            updateFields({
+              weather: { ...weather, weather: WeatherTypeGuard(e) },
+            });
+          }}
+        >
+          <Radio.Option value={"sun"}>{t("맑음")}</Radio.Option>
+          <Radio.Option value={"fog"}>{t("흐림")}</Radio.Option>
+          <Radio.Option value={"rain"}>{t("비")}</Radio.Option>
+          <Radio.Option value={"snow"}>{t("눈")}</Radio.Option>
+        </Radio>
       </FormLayout.Main>
     </FormLayout>
   );
