6b4a80ff9c0754ac4477065551518094284a16d7
Jungu Lee <1zzangjun@gmail.com>
2023-02-27 19:40:51 +0900

    :art: 모달, 뒤로가기 방지기능 추가

--- client/src/components/GlobalModal/Modal.tsx
+++ client/src/components/GlobalModal/Modal.tsx
@@ -3,18 +3,28 @@ import {
   modalComponentAtom,
 } from "@/recoil/atom/globalModalAtom";
 import styled from "@emotion/styled";
+import { useEffect } from "react";
 import { useRecoilValue, useRecoilState } from "recoil";
-
-import { Button } from "../atom/Button";
 import Portal from "./Portal";
 
 export const Modal = () => {
   const modalElem = useRecoilValue(modalComponentAtom);
   const [isModalOpen, setIsModalOpen] = useRecoilState(isModalOpenAtom);
 
+  useEffect(() => {
+    return () => {
+      window.removeEventListener("popstate", ()=>setIsModalOpen(false));
+    };
+  }, []);
+
   return isModalOpen ? (
     <Portal>
-      <ModalBackDrop onClick={() => setIsModalOpen((prev) => !prev)}>
+      <ModalBackDrop
+        onClick={() => {
+          history.back();
+          setIsModalOpen(false);
+        }}
+      >
         <ModalWrapper
           onClick={(e) => {
             e.stopPropagation();
@@ -28,8 +38,8 @@ export const Modal = () => {
   ) : null;
 };
 const ModalWrapper = styled.div`
-  width: 80vw;
-  height: 80vh;
+  width: calc(100% - 32px);
+  height: calc(100% - 32px);
   position: relative;
   padding: 16px;
   background-color: var(--pure-white);
--- client/src/hooks/useModal.tsx
+++ client/src/hooks/useModal.tsx
@@ -10,6 +10,10 @@ const useModal = () => {
   
   /** 모달로 사용할 컴포넌트를 인자로 받아 모달을 생성 */
   const onClickModal = (component: React.ReactNode) => {
+    
+    history.pushState(null, "", location.href);
+    window.addEventListener("popstate", ()=>setIsModalOpen(false));
+
     if (ModalComponent !== component) {
       setModalComponent(component);
     }
