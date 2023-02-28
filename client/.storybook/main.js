6e5201e93ccab90a208b49a66d5752c6c7ff54bf
Jungu Lee <1zzangjun@gmail.com>
2023-02-28 20:44:32 +0900

    :art: storybook 추가

--- client/.storybook/main.js
+++ client/.storybook/main.js
@@ -6,12 +6,14 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    
   ],
   framework: "@storybook/react",
   core: {
     builder: "@storybook/builder-webpack5",
   },
   webpackFinal: async (config) => {
+
     config.resolve.plugins = [new TsconfigPathsPlugin()];
     return config;
   },
---
+++ client/src/stories/components/Checkbox.stories.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import Checkbox from "@/components/atom/Checkbox";
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: "Components/Checkbox",
+  component: Checkbox,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    label: { control: "text" },
+    defaultChecked: { control: "boolean" },
+  },
+} as ComponentMeta<typeof Checkbox>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof Checkbox> = (args) => (
+  <Checkbox {...args} />
+);
+
+export const Checked = Template.bind({});
+export const UnChecked = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+
+Checked.args = {
+  defaultChecked: true,
+  label: "라벨입니다",
+};
+
+UnChecked.args = {
+  defaultChecked: false,
+  label: "라벨입니다",
+};
