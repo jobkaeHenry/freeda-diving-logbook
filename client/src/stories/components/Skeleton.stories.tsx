1fd0fde9aff32093945c31de00ba9d6a0e0edbe8
Jungu Lee <1zzangjun@gmail.com>
2023-03-01 17:20:00 +0900

    :sparkles: 스토리북 추가

---
+++ client/src/stories/components/Skeleton.stories.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import Skeleton from "@/components/atom/Skeleton";
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: "Components/Skeleton",
+  component: Skeleton,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    width: { control: "text" },
+    height: { control: "text" },
+  },
+} as ComponentMeta<typeof Skeleton>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof Skeleton> = (args) => (
+  <>
+    <Skeleton {...args} />
+  </>
+);
+
+export const Default = Template.bind({});
+
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+
+Default.args = {
+  width: "300px",
+  height: "200px",
+};
