5c650ab915b4cb1a945ebf8a40e17fdd20a772df
Jungu Lee <1zzangjun@gmail.com>
2023-03-02 22:31:59 +0900

    :art: storybook SVG 웹팩 설정 추가

--- client/.storybook/main.js
+++ client/.storybook/main.js
@@ -6,15 +6,17 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    
   ],
   framework: "@storybook/react",
   core: {
     builder: "@storybook/builder-webpack5",
   },
   webpackFinal: async (config) => {
-
     config.resolve.plugins = [new TsconfigPathsPlugin()];
+    const rules = config.module.rules;
+    const fileLoaderRule = rules.find((rule) => rule.test.test(".svg"));
+    fileLoaderRule.exclude = /\.svg$/;
+    rules.push({ test: /\.svg$/, use: ["@svgr/webpack"] });
     return config;
   },
 };
