9f02ab50925ed612f9dffd38adf09e909a8b5bdc
Jungu Lee <1zzangjun@gmail.com>
2023-03-02 21:27:40 +0900

    :art: usecallback 사용

--- client/src/pages/newlog/index.tsx
+++ client/src/pages/newlog/index.tsx
@@ -6,11 +6,13 @@ import { FormEvent, useCallback, useState } from "react";
 import PaddingLayout from "@/features/newlogs/layout/PaddingLayout";
 import DiveTypeForm from "@/features/newlogs/pages/DiveTypeForm";
 import useMultistepForm from "@/hooks/useMultistepForm";
+
 import LocationForm from "@/features/newlogs/pages/LocationForm";
 import DepthForm from "@/features/newlogs/pages/DepthForm";
 import AirUsageForm from "@/features/newlogs/pages/AirUsageForm";
 import GearForm from "@/features/newlogs/pages/GearForm";
 import PersonalForm from "@/features/newlogs/pages/PersonalForm";
+
 import scubaDiveModel from "@/features/newlogs/data/scubaDiveModel";
 import { DiveLogTypes } from "@/types/DiveLogTypes";
 import axios, { axiosPrivate } from "@/lib/api/axios";
@@ -22,6 +24,14 @@ import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { useTranslation } from "next-i18next";
 import { createLogServer } from "@/data/URL/server/newlog/createLog";
 
+import React from "react";
+
+// const LocationForm = React.lazy(() => import('@/features/newlogs/pages/LocationForm'));
+// const DepthForm = React.lazy(() => import('@/features/newlogs/pages/DepthForm'));
+// const AirUsageForm = React.lazy(() => import('@/features/newlogs/pages/AirUsageForm'));
+// const GearForm = React.lazy(() => import('@/features/newlogs/pages/GearForm'));
+// const PersonalForm = React.lazy(() => import('@/features/newlogs/pages/PersonalForm'));
+
 export async function getStaticProps(context: GetStaticPropsContext) {
   const locale = context.locale;
   return {
@@ -30,12 +40,12 @@ export async function getStaticProps(context: GetStaticPropsContext) {
         "common",
         "diveForm",
       ])),
-      locale
+      locale,
     },
   };
 }
 
-type Props = { existingData?: DiveLogTypes, locale?:string };
+type Props = { existingData?: DiveLogTypes; locale?: string };
 
 const DiveForm = ({ existingData }: Props) => {
   const [data, setData] = useState(
@@ -59,20 +69,22 @@ const DiveForm = ({ existingData }: Props) => {
       <GearForm {...data} updateFields={updateField} />,
       <PersonalForm {...data} updateFields={updateField} />,
     ],
+    // 서버측에서 받은 데이터가 있다면, 커스터마이징 부분으로 넘김
     existingData ? 3 : 0
   );
 
-  const onSubmit = (e: FormEvent) => {
+  const onSubmit = useCallback((e: FormEvent) => {
     e.preventDefault();
     if (isLastStep) {
       axios.post(createLogServer, data).then((res) => {
-      // axiosPrivate.post(createLog, data).then((res) => {
+        // axiosPrivate.post(createLog, data).then((res) => {
         console.log(res);
         router.replace(`/${getDiveLog}/${res.data.id}`);
       });
     }
     next();
-  };
+  }, [isLastStep]);
+  
   return (
     <>
       <Navbar />
@@ -80,7 +92,6 @@ const DiveForm = ({ existingData }: Props) => {
         <form onSubmit={onSubmit}>
           {/* 실제 폼 */}
           {step}
-
           {/* 버튼 */}
           <FixedBottomCTA>
             {!isFirstStep ? (
