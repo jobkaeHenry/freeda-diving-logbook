ee2ca6f7d819ad516e7017a4155c4fc7fdd05a7a
Jungu Lee <1zzangjun@gmail.com>
2023-03-03 22:18:45 +0900

    :sparkles: 다이브로그 삭제 기능 추가

--- server/routers/diveLogRoute.ts
+++ server/routers/diveLogRoute.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import {
   createDiveLog,
+  deleteDivelogById,
   getDiveLogById,
   getDiveLogsByUserId,
 } from "../controllers/diveLog-controller";
@@ -15,4 +16,5 @@ const router = express.Router();
 router.get("/:id", getDiveLogById);
 router.get("/", getDiveLogsByUserId);
 router.post("/", createDiveLog);
+router.delete("/:id",deleteDivelogById)
 export default router;
