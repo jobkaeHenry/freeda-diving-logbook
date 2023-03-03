940ea739754eba3887c013facb4603c4f29ca4a7
Jungu Lee <1zzangjun@gmail.com>
2023-03-03 17:21:40 +0900

    :sparkles: mongoose unique validator 추가

---
+++ server/models/user.ts
@@ -0,0 +1,15 @@
+import { model, Schema } from "mongoose";
+import uniqueValidator from "mongoose-unique-validator"
+
+const userSchema = new Schema({
+  // unique 를 추가 할 경우, 이메일에 고유 키값을 생성해 빠르게 찾을 수 있음
+  email: { type: String, require: true, unique: true },
+  password: { type: String, require: true, minlength: 8 },
+  nickName: { type: String, require: true },
+  image: { type: String, require: false },
+  divelog: { type: String, require: true },
+});
+// 고유 값인지 검증하는 mongoose Validator
+userSchema.plugin(uniqueValidator)
+
+export const User = model("User", userSchema);
