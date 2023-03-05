e4fde3ae8c572a981574dd3f5055b5307bb2fd5f
Jungu Lee <1zzangjun@gmail.com>
2023-03-05 19:58:42 +0900

    :minor: 사용하지 않는 모듈 제거

--- server/controllers/user-Controller.ts
+++ server/controllers/user-Controller.ts
@@ -1,5 +1,4 @@
 import { Request, Response, NextFunction } from "express";
-import { dummy } from "../models/dummy";
 import HttpError from "../models/error";
 import { User } from "../models/user";
 
