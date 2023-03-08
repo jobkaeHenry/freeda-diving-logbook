42ef07475fce724599a4b800d503e822dc7a0887
Jungu Lee <1zzangjun@gmail.com>
2023-03-09 03:11:19 +0900

    :art: refresh 토큰 기능 추가

--- server/controllers/user-Controller.ts
+++ server/controllers/user-Controller.ts
@@ -4,6 +4,10 @@ import { User } from "../models/user";
 import bcrypt from "bcryptjs";
 import jwt, { Secret } from "jsonwebtoken";
 import { createAccToken, createRefreshToken } from "../utils/tokenGenerator";
+import dotenv from "dotenv";
+
+dotenv.config();
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
 
 export const signUp = async (
   req: Request,
@@ -48,8 +52,6 @@ export const signUp = async (
   const accessToken = createAccToken(createdUser.id);
   const refreshToken = createRefreshToken(createdUser.id);
 
-  createdUser.refreshToken = refreshToken;
-
   try {
     await createdUser.save();
   } catch (err) {
@@ -58,7 +60,7 @@ export const signUp = async (
   }
 
   res.status(201).json({
-    userId: createdUser.id,
+    userId: createdUser._id,
     nickName: createdUser.nickName,
     image: createdUser.image,
     accessToken,
@@ -105,7 +107,14 @@ export const login = async (
 
   const accessToken = createAccToken(existingUser.id);
   const refreshToken = createRefreshToken(existingUser.id);
-  
+
+  existingUser.refreshToken = refreshToken;
+  try {
+    existingUser.save();
+  } catch (err) {
+    return next(new HttpError("토큰 저장에 실패했습니다", 500));
+  }
+
   res.status(200).json({
     userId: existingUser.id,
     nickName: existingUser.nickName,
@@ -115,19 +124,51 @@ export const login = async (
   });
 };
 
-export const tokenRefresh = (
+export const tokenRefresh = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
   const { refreshToken } = req.body;
+
   //@ts-expect-error
-  jwt.verify(refreshToken, JWT_SECRET_KEY as Secret, (err, decoded) => {
+  jwt.verify(refreshToken, JWT_SECRET_KEY as Secret, async (err, decoded) => {
     if (err) {
       return next(new HttpError("유효하지 않은 토큰입니다", 401));
     }
+
     const { userId } = decoded;
-    const accessToken = createAccToken(userId);
-    res.status(200).json({ accessToken });
+
+    let existingUser;
+    try {
+      existingUser = await User.findOne({ _id: userId });
+    } catch (err) {
+      const error = new HttpError("내부적 오류", 500);
+      return next(error);
+    }
+
+    if (!existingUser) {
+      const error = new HttpError("해당 토큰ID의 유저가 존재하지않습니다", 404);
+      return next(error);
+    } else if (existingUser) {
+      if (existingUser.refreshToken !== refreshToken) {
+        const error = new HttpError("유효하지 않은 토큰입니다", 401);
+        return next(error);
+      }
+
+      const newAccessToken = createAccToken(userId);
+      const newRefreshToken = createRefreshToken(userId);
+      existingUser.refreshToken = newRefreshToken;
+      try {
+        await existingUser.save();
+      } catch (err) {
+        const error = new HttpError("유저 생성에 실패했습니다", 500);
+        next(error);
+      }
+
+      res
+        .status(200)
+        .json({ accessToken: newAccessToken, refreshToken: newRefreshToken });
+    }
   });
 };
--- server/utils/tokenGenerator.ts
+++ server/utils/tokenGenerator.ts
@@ -1,11 +1,10 @@
-import dotenv from "dotenv";
 import jwt, { Secret } from "jsonwebtoken";
+import dotenv from "dotenv";
 
 dotenv.config();
 const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
 
 export const createAccToken = (userId: string, expiresIn = "1h") => {
-  
   return jwt.sign({ userId }, JWT_SECRET_KEY as Secret, { expiresIn });
 };
 
